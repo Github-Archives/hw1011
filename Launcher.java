8d2cedcc451ebeb0efc669df4396aad3c21c43d1
Drew Williams <15261678+Github-Archives@users.noreply.github.com>
2020-10-11 04:07:21 -0400

    Update Launcher.java

--- Launcher.java
+++ Launcher.java
@@ -1,7 +1,3 @@
-// Drew Williams
-// hw1011
-// https://github.com/Github-Archives/hw1011/tree/master
-
 import java.util.Random;
 
 public class Launcher {
@@ -18,27 +14,21 @@ public class Launcher {
         Random rnd = new Random();
         rnd.setSeed(3);
 
-        //System.out.println("Repeat These Things (x) times");
         for(int i = 0; i < 3; i++) {
             min = 1;
             max = 5;
             xRandom = rnd.nextInt((max - min) + 1) + min;
-            //System.out.println("(x) = " + xRandom);
-            //System.out.println("Insert These Randoms into the queue..");
 
             for(int x = 0; x < xRandom; x++) {
                 min = 0;
                 max = 99;
                 aRandom = rnd.nextInt((max - min) + 1) + min;
-                //System.out.println("(Random Num.) = " + aRandom);
                 q.enQueue(aRandom);
             }
             min = 1;
             max = 5;
             yRandom = rnd.nextInt((max - min) + 1) + min;
-            //System.out.println("(y) = " + yRandom);
             for(int y = 0; y < yRandom; y++) {
-                //System.out.println(q.deQueue());
                 q.deQueue();
             }
             //q.displayQueue();
